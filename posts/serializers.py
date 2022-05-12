eee355d8f34ce188ffe9bb27f7120f8da0384f3b
Tim Schulz <tim@getbej.com>
2022-05-12 11:34:00 +0000

    Get like_id andfollowing_id in serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     # validation function convention: validate_field-to-validate
     def validate_image(self, value):
@@ -24,13 +26,23 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated: 
+            liked = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return liked.id if liked else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title', 'content',
-            'image', 'is_owner', 'profile_id', 'profile_image', 'image_filter'
+            'image', 'is_owner', 'profile_id', 'profile_image', 'image_filter', 
+            'like_id'
         ]
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
